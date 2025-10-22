d5bcb59134b908de109fbb5cbde7c745fd0eb408
stefphd <stefano.lovato@unipd.it>
2025-10-22 10:10:10 +0200

    Test with mex

---
+++ sumScalar.c
@@ -0,0 +1,18 @@
+/* sumScalar.c */
+#include "mex.h"
+
+void mexFunction(int nlhs, mxArray *plhs[],
+                 int nrhs, const mxArray *prhs[])
+{
+    if (nrhs != 2) {
+        mexErrMsgIdAndTxt("mymex:args", "Two inputs required.");
+    }
+    if (!mxIsDouble(prhs[0]) || !mxIsDouble(prhs[1])) {
+        mexErrMsgIdAndTxt("mymex:types", "Inputs must be double.");
+    }
+
+    double a = mxGetScalar(prhs[0]);
+    double b = mxGetScalar(prhs[1]);
+
+    plhs[0] = mxCreateDoubleScalar(a + b);
+}
---
+++ testSumScalar.m
@@ -0,0 +1,11 @@
+
+% testSumScalar.m
+
+disp('Running MATLAB script')
+
+a = 2;
+b = 3;
+s = sumScalar(a, b);
+disp(['Sum is: ' num2str(a) '+' num2str(b) '=' num2str(s)])
+
+disp('Script end')
